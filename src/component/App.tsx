2248ffa7d701f1dd602b219a1f5c5d1b45c50767
Sebastian Corral <sebastrese@outlook.com>
2020-06-09 20:07:23 -0600

    Changed the landing page

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Components
-import Shell from './shell/Shell';
+import Shell from "./shell/Shell";
+import Landing from "./landing/Landing";
 
 // Styles
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <Router>
       <div className="wrapper flex flex-col w-screen h-screen">
-        <Shell />
+        <Landing />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
