c8850e29f217a7bfbbc7587a311c626c47613eb8
eduricharte <45698084+eduricharte@users.noreply.github.com>
2020-06-15 16:05:48 -0600

    Modified the Landing Page

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -75,29 +75,6 @@ function Landing() {
           </div>
         </div>
       </div>
-      {/**Powered by */}
-      <div className="flex flex-col w-full h-full items-center ">
-        <div className="divide-y-2 divide-white-400">
-          <div
-            className=" flex text-center pb-3 h-1/2 px-30 pt-16 text-4xl font-black"
-            style={{ fontFamily: "Muli, sans-serif" }}
-          >
-            <span>Powered by</span>
-          </div>
-
-          <div className="grid grid-cols-3 gap-8 flex content-center h-1/2 px-30  pt-16 pb-16  ">
-            <div>
-              <img src={Amazon_logo} width="200" height=""></img>
-            </div>
-            <div>
-              <img src={Google} width="200" height=""></img>
-            </div>
-            <div>
-              <img src={Spotify} width="200" height=""></img>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="space-y-6 flex flex-row h-auto px-20 py-12 text-white bg-blue-600">
         <div className="w-3/5">
           <div className="font-family: Muli, font-extrabold text-4xl">
@@ -121,71 +98,108 @@ function Landing() {
           </div>
         </div>
         <div className="content-center -w-2/5">
-          <img src={Reading} width="391.32px" height="464px" />
+          <img src={Reading} width="391" height="" />
         </div>
       </div>
 
-      <div className="flex flex-col w-full font-family: Muli font-extrabold content-center text-4xl h-24 divide-y divide-black px-64">
-        <div className="text-center">Features</div>
-        <div />
+        {/**Powered by */}
+        <div className="flex flex-col w-full h-full items-center">
+            <div className="divide-y-2 divide-black">
+                <div className=" flex text-center pb-3 h-1/2 px-30 pt-16 text-4xl font-black" style={{ fontFamily: "Muli, sans-serif" }}>
+                    <span>Features</span>
+                </div>
+                <div className="flex content-center h-1/2 px-30 pt-16" />
+            </div>
+        </div>
+
+        {/**Blazingly Fast */}
+        <div className="flex flex-row">
+            <div className="flex flex-row bg-pink-200 px-6 py-6">
+                <div className="content-center self-center">
+                    <img src={Fast} width="" height="333" />
+                </div>
+            <div className="text-pink-600 -w-1/2">
+                <div className='divide-y-2 divide-pink-600 space-y-6'>
+                    <div style={{ fontFamily: "Muli, sans-serif" }} className="font-extrabold text-4xl">
+                        Blazingly Fast
+                    </div>
+                    <div style={{ fontFamily: "Lato, sans-serif" }} className='pt-6'>
+                        <b>Search speed,</b> our platform is made to perform advanced
+                        searches quickly so you can enjoy your content faster.
+                    <br />
+                    <br />
+                        Powered by <b>Alexandria Neutrino</b>.
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            {/**Trends */}
+        <div className="flex flex-row">
+            <div style={{backgroundColor: "#283593"}} className="flex flex-row px-6 py-6">
+                <div className="content-center self-center">
+                    <img src={Trends} width="" height="333" />
+                </div>
+            <div className="text-pink-300 -w-1/2">
+                <div className='divide-y-2 divide-pink-300 space-y-6'>
+                    <div style={{ fontFamily: "Muli, sans-serif" }} className="font-extrabold text-4xl">
+                        Trends
+                    </div>
+                    <div style={{ fontFamily: "Lato, sans-serif" }} className='pt-6'>
+                        <b>Get the latest topic</b>, our platform brings you the possibility to look at the newer things happening around you.
+                    <br />
+                    <br />
+                    <i>#AlexandriaIsAwesome</i>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+            </div>
+
+        {/**Accesibility */}
+        <div style={{backgroundColor: "#006064"}} className="space-y-6 flex flex-row pl-20 py-12 text-white bg-teal-600">
+            <div className="content-center">
+                <img src={Files} width="340" height="" />
+            </div>
+            <div className="pl-32 divide-y-2 content-center self-center">
+                <div style={{ fontFamily: "Muli, sans-serif" }} className="font-extrabold text-4xl pb-2">
+                    Accessibility
+                </div>
+            <div style={{ fontFamily: "Lato, sans-serif" }} className="w-2/3 pt-6">
+                    <b>Don’t get stuck</b>, our platform offers new ways to get inside
+                    the digital age. From paper to Alexandria.
+                <br />
+                <br />
+                    <i>From paper to Alexandria.</i>
+            </div>
+            </div>
+        </div>
+
+      {/**Powered by */}
+      <div className="flex flex-col w-full h-full items-center">
+        <div className="divide-y-2">
+          <div
+            className=" flex justify-center content-center pb-3 h-1/2 px-30 pt-16 text-4xl font-black"
+            style={{ fontFamily: "Muli, sans-serif" }}
+          >
+            <span>Powered by</span>
+          </div>
+
+          <div className="grid grid-cols-3 gap-8 flex content-center h-1/2 px-30  pt-16 pb-16  ">
+            <div>
+              <img src={Amazon_logo} width="200" height=""></img>
+            </div>
+            <div>
+              <img src={Google} width="200" height=""></img>
+            </div>
+            <div>
+              <img src={Spotify} width="200" height=""></img>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-row">
-        <div className="flex flex-row bg-pink-200">
-          <div className="content-center ml-16 self-center">
-            <img src={Fast} width="320px" height="333px" />
-          </div>
-          <div className="text-pink-600 space-y-6 mr-16 my-16 -w-1/2">
-            <div className="font-family: Muli font-extrabold text-4xl">
-              Blazingly Fast
-            </div>
-            <div className="font-family: Lato space-y-6">
-              <b>Search speed,</b> our platform is made to perform advanced
-              searches quickly so you can enjoy your content faster.
-            </div>
-            <div>
-              Powered by <b>Alexandria Neutrino</b>.
-            </div>
-          </div>
-        </div>
-        <div className="bg-purple-900 flex flex-row">
-          <div className="content-center ml-16 self-center w-1/2">
-            <img src={Trends} width="297.62px" height="333px" />
-          </div>
-          <div className="text-pink-300 space-y-6 mr-16 my-16 -w-1/2">
-            <div className="font-family: Muli font-extrabold text-3xl">
-              Trends
-            </div>
-            <div className="font-family: Lato space-y-6">
-              <b>Get the latest topic</b>, our platform brings you the
-              possibility to look at the newer things happening around you.
-            </div>
-            <div>
-              <i>#AlexandriaIsAwesome</i>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-6 flex flex-row pl-20 py-12 text-white bg-teal-600">
-        <div className="content-center">
-          <img src={Files} width="340.14px" height="396.64px" />
-        </div>
-        <div className="pl-32">
-          <div className="font-family: Muli, font-extrabold text-4xl">
-            Accessibility
-          </div>
-          <div className="text-xl font-bold space-y-3">________</div>
-          <br />
-          <div className="space-y-6 font-family:lato w-2/3">
-            <div>
-              <b>Don’t get stuck</b>, our platform offers new ways to get inside
-              the digital age. From paper to Alexandria.
-            </div>
-            <div>From paper to Alexandria</div>
-          </div>
-        </div>
-      </div>
       {/*Footer*/}
 
       <div
@@ -227,24 +241,24 @@ function Landing() {
           </div>
           <div className="grid grid-cols-3 col-start-3 gap-4">
             <div className="flex content-center">
-              <img src={Facebook_logo} width="20" height="20"></img>
+                <img src={Facebook_logo} width="20" height="20"></img>
             </div>
             <div className="flex content-center">
-              <img src={Twitter_logo} width="20" height="20"></img>
+                <img src={Twitter_logo} width="20" height="20"></img>
             </div>
             <div className="flex content-center">
-              <img src={Instagram_logo} width="20" height="20"></img>
+                <img src={Instagram_logo} width="20" height="20"></img>
             </div>
           </div>
           <div className="col-start-5 font-bold">Contact</div>
-          <div className="grid grid-cols-2 col-start-5 gap-2">
-            <div className="flex content-center">
-              <img src={Mail} width="20" height="20"></img>
+            <div className="grid grid-cols-2 col-start-5 gap-2">
+                <div className="flex content-center">
+                    <img src={Mail} width="20" height="20"></img>
+                </div>
+                <div className="flex content-center">
+                    <img src={Phone} width="20" height="20"></img>
+                </div>
             </div>
-            <div className="flex content-center">
-              <img src={Phone} width="20" height="20"></img>
-            </div>
-          </div>
           <div className="col-start-6 font-bold">Country</div>
           <div className="grid grid-cols-2 col-start-6 content-around">
             <div className="flex content-center">
